47f96e6c8170940ce375723ede2e14f72f13dbb2
ck1909 <kakatkar.chinmay@gmail.com>
2014-04-16 16:33:28 +0200

    full version of clean data assignment

--- CodeBook.md
+++ CodeBook.md
@@ -1,4 +1,28 @@
 getcleandata
 ============
 
-Variables:
+## Variables:
+- The variables (column names) specify the type of features captured by the smartphone sensors.
+- Please see features_info.txt for more details.
+
+## Data
+- The sensor data was provided in a txt file for the test and training datasets (X_test.txt and X_train.txt, respectively). The data was given in exponential form (e.g. "2.5717778e-001").
+- Feature names were provided in a separate file for the test and training datasets (Y_test.txt and Y_train.txt, respectively).
+
+## Data Cleaning Transformations
+The data cleaning steps are encapsulated in functions in the script "run_analysis.R".
+
+*read_dataset_xy()*
+- Combines sensor data and the corresponding feature names.
+- Dataset is cropped to retain only the mean and std data, to give clean dataset.
+- The activity numbers in the raw data are replaced with more descriptive activity names.
+
+*merge_test_train()*
+- A clean training dataset is appended below a clean test dataset.
+- Assumes that datasets passed to this function are in fact clean.
+
+*main()*
+- Encapsulates the high-level data cleaning function sequence, and returns the tidy dataset.
+- To keep the call to main() simple, file names of input data are hardcoded inside the function body instead of passing them as arguments to main().
+- It is assumed that all necessary input files are at the root of the working directory.
+- For convenience, the tidy dataset is written out to txt file ("tidy_dataset.txt").
--- run_analysis.R
+++ run_analysis.R
@@ -2,20 +2,20 @@
 
 read_dataset_xy <- function(f_x, names_x, f_y, names_y, f_labels) {
 	#read x and y data
-	#only relevant columns 1:6 contain avg and std data
-	#give relevant columns readable names
-	#ensure remaining columns are skipped (set colClass to "NULL")
-	mycolclass <- c(rep("numeric", 6), rep("NULL", 555))
-	x <- read.table(f_x, colClasses = mycolclass)
-	colnames(x) <- names_x
+    x <- read.table(f_x, col.names = names_x)
+    #store only columns containing mean and std data
+    x <- subset(x, select=grep("mean()|std()", colnames(x)))
+    #colnames(x) <- names_x
 	y <- read.table(f_y, col.names = names_y)
+    #get activity labels
 	labels <- read.table(f_labels)
 	
-	#convert y vals to human-readable chars
-	lc <- as.character(labels$V2)
+	#convert y vals to human-readable chars	
+    lc <- as.character(labels$V2)
 	y$activity <- as.character(y$activity)
 	for(i in 1:nrow(labels)) y$activity[which((y$activity == labels$V1[i]))] <- lc[i]
-	#combine x and y data
+	
+    #combine x and y data
 	x <- cbind(x, y)
 	return(x)
 }
@@ -34,19 +34,25 @@ main <- function() {
 	f_x_train <- paste(wd, "/X_train.txt", sep="")
 	f_y_train <- paste(wd, "/Y_train.txt", sep="")
 	f_labels <- paste(wd, "/activity_labels.txt", sep="")
+	f_features <- paste(wd, "/features.txt", sep="")
 	f_tidy <- paste(wd, "/tidy_dataset.txt", sep="")
 
 	#prepare column names
-	names_x <- c("tBodyAcc-mean()-X", "tBodyAcc-mean()-Y", "tBodyAcc-mean()-Z", "tBodyAcc-std()-X", "tBodyAcc-std()-Y", "tBodyAcc-std()-Z")
+	features <- read.table(f_features)
+    names_x <- features$V2
 	names_y <- c("activity")
 	
+    #read datasets
 	x_test <- read_dataset_xy(f_x_test, names_x, f_y_test, names_y, f_labels)
 	x_train <- read_dataset_xy(f_x_train, names_x, f_y_train, names_y, f_labels)
-	x_merged <- merge_test_train(x_test, x_train)
+	
+    #merge datasets
+    x_merged <- merge_test_train(x_test, x_train)
 	
     #write tidy dataset to txt file (optional)
 	write.table(x_merged, f_tidy, row.names=F)
     
     #return tidy dataset
+    #return(x_test)
 	return(x_merged)
 }
